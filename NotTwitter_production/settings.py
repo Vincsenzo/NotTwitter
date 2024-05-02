b5f371c00352a0eeb6630543c9c7f72da59a8151
Vincsenzo <szabo.vince@icloud.com>
2024-05-02 13:14:55 +0200

    it should work now with environments

--- NotTwitter_production/settings.py
+++ NotTwitter_production/settings.py
@@ -3,6 +3,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# environ activeted
 import environ
 
 env = environ.Env()
